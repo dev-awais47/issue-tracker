47c8ce53a9baa0512eeac021d66fa38054104354
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-06-14 16:17:58 +0500

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,35 +1,45 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface issueForm {
-  title: string;
-  description: string;
-}
+type issueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<issueForm>();
+  const { register, control, handleSubmit, formState: {errors} } = useForm<issueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
+  const [error, setError] = useState();
   return (
     <form
       className="max-w-xl space-y-3"
       onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data);
-        router.push('/issues')
+        try {
+          await axios.post('/api/issues', data);
+          router.push('/issues')
+        } catch (error) {
+          // setError("An unexpected error occurs");
+        }
       })}
     >
       <TextField.Root placeholder="Title" size="3" {...register("title")} />
+      { errors.title && <Text color="red" as="p">{errors.title.message}</Text> }
       <Controller
         name="description"
         control={control}
         render={({ field }) => (
           <SimpleMDE placeholder="Description" {...field} />
         )}
-      />
+        />
+        { errors.description && <Text color="red" as="p">{errors.description.message}</Text> }
       <Button>Submit the Issue</Button>
     </form>
   );
