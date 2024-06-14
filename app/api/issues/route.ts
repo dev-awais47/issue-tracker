9dd7245f429121aea4c29519f9a53a69c7a6cac4
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-06-14 16:28:49 +0500

    Extract the Error Message component

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -11,8 +11,8 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
-    data: { title: body.title, description: body.description }
+    data: { title: body.title, description: body.description },
   });
 
-  return NextResponse.json(newIssue, {status: 201})
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,13 +9,19 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type issueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: {errors} } = useForm<issueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<issueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState();
   return (
@@ -23,23 +29,26 @@ const NewIssuePage = () => {
       className="max-w-xl space-y-3"
       onSubmit={handleSubmit(async (data) => {
         try {
-          await axios.post('/api/issues', data);
-          router.push('/issues')
+          await axios.post("/api/issues", data);
+          router.push("/issues");
         } catch (error) {
           // setError("An unexpected error occurs");
         }
       })}
     >
       <TextField.Root placeholder="Title" size="3" {...register("title")} />
-      { errors.title && <Text color="red" as="p">{errors.title.message}</Text> }
+
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
       <Controller
         name="description"
         control={control}
         render={({ field }) => (
           <SimpleMDE placeholder="Description" {...field} />
         )}
-        />
-        { errors.description && <Text color="red" as="p">{errors.description.message}</Text> }
+      />
+
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
       <Button>Submit the Issue</Button>
     </form>
   );
