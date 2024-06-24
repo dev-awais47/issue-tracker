346538de336219c9153ecd6eeec09caf3574b098
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-06-24 15:04:08 +0500

    Build the edit issue page

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,69 @@
+"use client";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+interface Props {
+  issue?: Issue;
+}
+
+const IssueForm = ({ issue }: Props) => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
+  const [error, setError] = useState<string | undefined>();
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("An unexpected error occurred.");
+    }
+  });
+
+  return (
+    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
+      <TextField.Root defaultValue={issue?.title} placeholder="Title" size="3" {...register("title")} />
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
+      <Controller
+        name="description"
+        control={control}
+        defaultValue={issue?.description}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+      <Button type="submit">Submit the Issue</Button>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </form>
+  );
+};
+
+export default IssueForm;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,63 +1,7 @@
-"use client";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import { createIssueSchema } from "@/app/validationSchemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import dynamic from "next/dynamic";
-import { z } from "zod";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
-
-type IssueForm = z.infer<typeof createIssueSchema>;
+import IssueForm from "../_components/IssueForm";
 
 const NewIssuePage = () => {
-  const router = useRouter();
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-
-  const [error, setError] = useState<string | undefined>();
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      await axios.post("/api/issues", data);
-      router.push("/issues");
-    } catch (error) {
-      setError("An unexpected error occurred.");
-    }
-  });
-
-  return (
-    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
-      <TextField.Root placeholder="Title" size="3" {...register("title")} />
-      <ErrorMessage>{errors.title?.message}</ErrorMessage>
-
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-      <Button type="submit">Submit the Issue</Button>
-
-      {error && <ErrorMessage>{error}</ErrorMessage>}
-    </form>
-  );
+  return <IssueForm />;
 };
 
 export default NewIssuePage;
