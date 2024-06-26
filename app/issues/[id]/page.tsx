04f21d68d96d2e3be20021f937eb39ccc26e01a2
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-06-26 19:08:21 +0500

    Secure the application from unauthorized users

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,12 +6,16 @@ import ReactMarkDown from "react-markdown";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -30,15 +34,17 @@ const IssueDetailPage = async ({ params }: Props) => {
           <ReactMarkDown>{issue.description}</ReactMarkDown>
         </Card>
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <Button>
-            <Pencil2Icon />
-            <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-          </Button>
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <Button>
+              <Pencil2Icon />
+              <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+            </Button>
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
