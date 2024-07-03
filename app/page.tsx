836dbceac6310ce73829f346b1625dbc15c5699f
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-07-03 17:03:10 +0500

    Build the layout of Pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return (
-    <div>Hello World</div>
-  );
+  return <Pagination currentPage={2} itemCount={100} pageSize={10} />;
 }
