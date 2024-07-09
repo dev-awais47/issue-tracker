d66b80c42f41dcbae2b3e31b4b13ada2a035deb4
Muhammad Awais <muhammadawaisijaz010@gmail.com>
2024-07-09 11:54:37 +0500

    remove container from the layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -24,59 +24,57 @@ const NavBar = () => {
   const { status, data: session } = useSession();
   return (
     <nav className="border-b-2 mb-5 px-5 py-3 bg-zinc-200">
-      <Container>
-        <Flex justify="between">
-          <Flex align="center" gap="3">
-            <Link className="scale-150" href="/">
-              <AiFillBug />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classnames({
-                      "font-bold": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === 'loading' && <Skeleton width="3rem" /> }
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Sign out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Sign in</Link>
-            )}
-          </Box>
+      <Flex justify="between">
+        <Flex align="center" gap="3">
+          <Link className="scale-150" href="/">
+            <AiFillBug />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classnames({
+                    "font-bold": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </Flex>
-      </Container>
+        <Box>
+          {status === "loading" && <Skeleton width="3rem" />}
+          {status === "authenticated" && (
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar
+                  src={session.user!.image!}
+                  fallback="?"
+                  size="2"
+                  radius="full"
+                  className="cursor-pointer"
+                  referrerPolicy="no-referrer"
+                />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text size="2">{session.user!.email}</Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
+                  <Link href="/api/auth/signout">Sign out</Link>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          )}
+          {status === "unauthenticated" && (
+            <Link href="/api/auth/signin">Sign in</Link>
+          )}
+        </Box>
+      </Flex>
     </nav>
   );
 };
